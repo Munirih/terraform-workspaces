c96822aa6e0e33b0bef98f847e501156dc4089c9
Munirih Nganyade <linkmunirih@gmail.com>
2026-04-16 17:18:43 +0200

    fix versioning errors

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "demo-vpc-tfstate-muni-20260410"
+    bucket         = "demo-vpc-tfstate-YOURNAME"
     key            = "vpc-module/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "demo-vpc-terraform-locks"
