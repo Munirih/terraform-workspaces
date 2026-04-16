ffb2c5b58c95fd53a09895adf1ba3f8ee56744f2
Munirih Nganyade <linkmunirih@gmail.com>
2026-04-16 17:22:35 +0200

    remove backend file

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "demo-vpc-tfstate-YOURNAME"
-    key            = "vpc-module/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "demo-vpc-terraform-locks"
-    encrypt        = true
-  }
-}
